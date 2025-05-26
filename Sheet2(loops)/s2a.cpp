66a1ef066db1aeb9c780f2af23e9f5639b27f87c
Taj-kia <tasniaa1114@gmail.com>
2025-05-26 19:32:14 +0600

    Update s2a.cpp

--- Sheet2(loops)/s2a.cpp
+++ Sheet2(loops)/s2a.cpp
@@ -1,3 +1,8 @@
+// Problem Name : 1 to N
+//Course Title : Competitive Programming Sessional
+//Course Code : CCE-2310
+//Course Teacher Name : Mirza Raquib
+//Date : 24/03/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
