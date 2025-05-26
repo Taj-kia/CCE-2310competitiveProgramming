1e86ce2e2f5d115ddd95fa3ee912ba7cd189d7a3
Taj-kia <tasniaa1114@gmail.com>
2025-05-26 19:39:36 +0600

    Update s2v.cpp

--- Sheet2(loops)/s2v.cpp
+++ Sheet2(loops)/s2v.cpp
@@ -1,3 +1,8 @@
+// Problem Name : PUM
+//Course Title : Competitive Programming Sessional
+//Course Code : CCE-2310
+//Course Teacher Name : Mirza Raquib
+//Date : 24/03/25
 #include <bits/stdc++.h>
 using namespace std;
 
