e4f0efe98b28984619790f6d900a185eabf441fc
Taj-kia <tasniaa1114@gmail.com>
2025-05-26 19:32:52 +0600

    Update s2b.cpp

--- Sheet2(loops)/s2b.cpp
+++ Sheet2(loops)/s2b.cpp
@@ -1,3 +1,8 @@
+// Problem Name : Even Numbers
+//Course Title : Competitive Programming Sessional
+//Course Code : CCE-2310
+//Course Teacher Name : Mirza Raquib
+//Date : 24/03/25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
