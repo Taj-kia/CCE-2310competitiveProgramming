665569661cc64c5394224e53f1bd851bc609e6ec
Taj-kia <tasniaa1114@gmail.com>
2025-06-23 22:50:36 +0600

    Crated S3[K].cpp

---
+++ Sheet #3 (Arrays)/S3[K].cpp
@@ -0,0 +1,21 @@
+// Problem Name : Sum Digits
+//Course Title : Competitive Programming Sessional
+//Course Code : CCE-2310
+//Course Teacher Name : Mirza Raquib
+//Date : 23/06/25
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long N;
+    string num;
+    long long sum=0;
+    cin>>N>>number;
+    for (long long i=0; i<N; i++)
+    {
+        sum+=number[i]-'0';
+    }
+    cout<<sum<<endl;
+    return 0;
+}
+
